63ba391ddfa85cd0f83a19c86013219f53457754
JKW <jjw15287@gmail.com>
2025-10-26 08:31:36 +0900

    code로 조회 기능 추가 > Optional<CommonCode> findByCode(String code)

--- src/main/java/org/goodee/startup_BE/common/repository/CommonCodeRepository.java
+++ src/main/java/org/goodee/startup_BE/common/repository/CommonCodeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommonCodeRepository extends JpaRepository<CommonCode, Long> {
@@ -21,4 +22,6 @@ public interface CommonCodeRepository extends JpaRepository<CommonCode, Long> {
             @Param("keyword") String keyword
     );
 
+    // code 로 데이터 조회 (jkw 추가)
+    Optional<CommonCode> findByCode(String code);
 }
