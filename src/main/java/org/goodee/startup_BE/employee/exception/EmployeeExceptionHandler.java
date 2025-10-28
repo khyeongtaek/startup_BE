bfff5978bd711d55c817e269eb80798ffd78cbb5
kht <gudxor1541@naver.com>
2025-10-28 11:29:39 +0900

    EmployeeExceptionHandler.java - CommonExceptionHandler로 공통화 된  ResourceNotFoundException 삭제 및 EmployeeController에서만 작동되도록 assignableTypes 속성추가

--- src/main/java/org/goodee/startup_BE/employee/exception/EmployeeExceptionHandler.java
+++ src/main/java/org/goodee/startup_BE/employee/exception/EmployeeExceptionHandler.java
@@ -2,6 +2,8 @@ package org.goodee.startup_BE.employee.exception;
 
 
 import org.goodee.startup_BE.common.dto.APIResponseDTO;
+// 이 핸들러를 적용할 컨트롤러를 import 해야 함 (경로는 예시)
+import org.goodee.startup_BE.employee.controller.EmployeeController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -9,21 +11,13 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+// assignableTypes 속성에 적용할 컨트롤러 클래스를 지정
+@RestControllerAdvice(assignableTypes = EmployeeController.class)
 public class EmployeeExceptionHandler {
 
     // 400 Bad Request (이메일 중복)
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<APIResponseDTO<Void>> handleDuplicateEmailException(DuplicateEmailException e) {
-        APIResponseDTO<Void> response = APIResponseDTO.<Void>builder()
-                .message(e.getMessage())
-                .build();
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    // 400 Bad Request (잘못된 코드값 또는 리소스 조회 실패)
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<APIResponseDTO<Void>> handleResourceNotFoundException(ResourceNotFoundException e) {
         APIResponseDTO<Void> response = APIResponseDTO.<Void>builder()
                 .message(e.getMessage())
                 .build();
