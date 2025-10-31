a700758e37794070bf925c72a8fb19bdb5d2b2c8
jokiwan <jjw15287@gmail.com>
2025-10-31 15:23:23 +0900

    메일 등록 요청/응답 DTO, 메일 수정 요청 DTO 추가

---
+++ src/main/java/org/goodee/startup_BE/mail/dto/MailUpdateRequestDTO.java
@@ -0,0 +1,29 @@
+package org.goodee.startup_BE.mail.dto;
+
+import lombok.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Service
+@Builder
+@ToString(exclude = "attachmentFiles")
+public class MailUpdateRequestDTO {
+	private String title;
+	private String content;
+	
+	// 수신 타입별 리스트
+	private List<String> to;
+	private List<String> cc;
+	private List<String> bcc;
+	
+	// 업로드할 첨부파일
+	private List<MultipartFile> attachmentFiles;
+	
+	// 삭제할 첨부파일
+	private List<Long> deleteAttachmentFileIds;
+}
