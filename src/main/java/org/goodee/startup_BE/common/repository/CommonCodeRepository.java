256d1867182b8794afb7944d1e705e35181b09a0
ziwk13 <ziwk13@hanmail.com>
2025-11-06 14:09:49 +0900

    CommonCodeRepository.java - 삭제되지 않은 결재 양식 조회 쿼리 메소드 추가

--- src/main/java/org/goodee/startup_BE/common/repository/CommonCodeRepository.java
+++ src/main/java/org/goodee/startup_BE/common/repository/CommonCodeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommonCodeRepository extends JpaRepository<CommonCode, Long> {
@@ -15,7 +16,8 @@ public interface CommonCodeRepository extends JpaRepository<CommonCode, Long> {
             "  AND (c.value1 = :keyword OR " + // keyword와 정확히 일치
             "       c.value2 = :keyword OR " +
             "       c.value3 = :keyword) " +
-            "  AND c.isDeleted = false") // 삭제되지 않음
+            "  AND c.isDeleted = false")
+        // 삭제되지 않음
     List<CommonCode> findByCodeStartsWithAndKeywordExactMatchInValues(
             @Param("codePrefix") String codePrefix,
             @Param("keyword") String keyword
@@ -27,5 +29,8 @@ public interface CommonCodeRepository extends JpaRepository<CommonCode, Long> {
             "AND c.isDeleted = false " +
             "ORDER BY c.sortOrder ASC")
     List<CommonCode> findAllDepartments();
+
+    // 삭제 되지 않은 결재 양식 조회를 위한 쿼리 메소드
+    List<CommonCode> findByCodeDescriptionAndIsDeletedFalseOrderBySortOrderAsc(String codeDescription);
 
 }
