150310fd7c11afba8e68542907cc5c490345fa3d
jinseong125 <jinseong125@gmail.com>
2025-11-11 09:47:24 +0900

    CommonCodeRepository - findAllDepartments 제거. CommonCodeRepositoryTest, CommonCodeServiceImplTest - 메서드 호출시 반환될값 변경 CommonCodeServiceImpl - findAllDepartments -> findByCodeStartsWithAndIsDeletedFalse 변경

--- src/main/java/org/goodee/startup_BE/common/service/CommonCodeServiceImpl.java
+++ src/main/java/org/goodee/startup_BE/common/service/CommonCodeServiceImpl.java
@@ -19,7 +19,7 @@ public class CommonCodeServiceImpl implements CommonCodeService {
 
   @Override
   public List<CommonCode>  getAllDepartments() {
-    return commonCodeRepository.findAllDepartments();
+    return commonCodeRepository.findByCodeStartsWithAndIsDeletedFalse("DP");
   }
 
   @Override
