f204a6919ad375bb5b70bb919c7d84ee5e6bc35e
ziwk13 <ziwk13@hanmail.com>
2025-11-14 22:48:43 +0900

    ChatRoomRepository.java - 사용자 ID를 기준으로 1 : 1 채팅방 존재 여부 쿼리 추가

--- src/main/java/org/goodee/startup_BE/chat/repository/ChatRoomRepository.java
+++ src/main/java/org/goodee/startup_BE/chat/repository/ChatRoomRepository.java
@@ -2,7 +2,22 @@ package org.goodee.startup_BE.chat.repository;
 
 import org.goodee.startup_BE.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    /**
+     * 두 사용자 ID를 기준으로 1:1 채팅방(isTeam = false)이 존재하는지 확인합니다.
+     * ChatEmployee 테이블을 자체 조인하여 두 사용자가 동일한 1:1 채팅방을 공유하는지 찾습니다.
+     */
+    @Query("SELECT ce1.chatRoom FROM ChatEmployee ce1 " +
+            "JOIN ChatEmployee ce2 ON ce1.chatRoom = ce2.chatRoom " +
+            "WHERE ce1.employee.employeeId = :userId1 " +
+            "AND ce2.employee.employeeId = :userId2 " +
+            "AND ce1.chatRoom.isTeam = false")
+    Optional<ChatRoom> findExistingOneOnOneRoom(@Param("userId1") Long userId1, @Param("userId2") Long userId2);
 
 }
